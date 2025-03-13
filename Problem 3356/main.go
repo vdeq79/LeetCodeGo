ab3fb0fefd60dbd3eb3c89b1efad3d70534dc3b0
vdeq79 <xszh79@outlook.com>
2025-03-13 18:53:26 +0100

    Optimize Problem 3356

--- Problem 3356/main.go
+++ Problem 3356/main.go
@@ -2,36 +2,51 @@ package main
 
 import "fmt"
 
-func minZeroArray(nums []int, queries [][]int) int {
-	sum := 0
+func isZeroArray(nums []int, queries [][]int, k int) bool {
+	prefix := 0
+	diff := make([]int, len(nums) + 1)
+
+    for i:=0; i<k; i++ {
+		diff[queries[i][0]]+= queries[i][2] 
+		diff[queries[i][1]+1]-= queries[i][2]
+	}
 
 	for i:=0; i<len(nums); i++ {
-		sum += nums[i]
-	}
+		prefix += diff[i]
 
-	if sum == 0 {
-		return 0
-	}
-
-    for j, query := range queries {
-		for i:=query[0]; i<=query[1]; i++ {
-
-			if(nums[i] > 0){
-				substract := min(nums[i], query[2])
-				nums[i] -= substract
-				sum -= substract
-
-				if sum == 0 {
-					return j+1
-				}
-			}
-
+		if nums[i] > prefix {
+			return false
 		}
+
 	}
 
-	return -1
+	return true
+}
+
+
+func minZeroArray(nums []int, queries [][]int) int {
+
+    left, right := 0, len(queries)
+
+	if !isZeroArray(nums, queries, right) {
+		return -1
+	}
+
+	for left <= right {
+		mid := left + (right - left) / 2
+		if isZeroArray(nums, queries, mid) {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+
+	}
+
+	return left
 }
 
 func main() {
 	fmt.Println(minZeroArray([]int{2,0,2}, [][]int{{0,2,1},{0,2,1},{1,1,3}}))
+	fmt.Println(minZeroArray([]int{0}, [][]int{{0,0,1},{0,0,1},{0,0,3}}))
+
 }
