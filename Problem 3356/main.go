b62daabf1b713d76dfb063496c2dc9ce6a168deb
vdeq79 <xszh79@outlook.com>
2025-03-13 22:02:24 +0100

    Optimized Problem 3356

--- Problem 3356/main.go
+++ Problem 3356/main.go
@@ -2,51 +2,41 @@ package main
 
 import "fmt"
 
-func isZeroArray(nums []int, queries [][]int, k int) bool {
-	prefix := 0
-	diff := make([]int, len(nums) + 1)
-
-    for i:=0; i<k; i++ {
-		diff[queries[i][0]]+= queries[i][2] 
-		diff[queries[i][1]+1]-= queries[i][2]
-	}
-
-	for i:=0; i<len(nums); i++ {
-		prefix += diff[i]
-
-		if nums[i] > prefix {
-			return false
-		}
-
-	}
-
-	return true
-}
-
-
 func minZeroArray(nums []int, queries [][]int) int {
 
-    left, right := 0, len(queries)
+	diff := make([]int, len(nums) + 1)
+	prefix := 0
+	k := 0
 
-	if !isZeroArray(nums, queries, right) {
-		return -1
-	}
+    for i:=0; i<len(nums); i++ {
+		prefix += diff[i]
 
-	for left <= right {
-		mid := left + (right - left) / 2
-		if isZeroArray(nums, queries, mid) {
-			right = mid - 1
-		} else {
-			left = mid + 1
+		for nums[i] > prefix && k < len(queries) {
+
+			if queries[k][1] >= i{
+
+				if (queries[k][0] <= i){
+					diff[i] += queries[k][2]
+					prefix += queries[k][2]
+				} else {
+					diff[queries[k][0]] += queries[k][2]
+				}
+
+				diff[queries[k][1] + 1] -= queries[k][2]
+			}
+
+			k++
 		}
 
+		if nums[i] > prefix {
+			return -1
+		}
 	}
 
-	return left
+	return k
 }
 
 func main() {
-	fmt.Println(minZeroArray([]int{2,0,2}, [][]int{{0,2,1},{0,2,1},{1,1,3}}))
-	fmt.Println(minZeroArray([]int{0}, [][]int{{0,0,1},{0,0,1},{0,0,3}}))
+	fmt.Println(minZeroArray([]int{0,10}, [][]int{{0,1,2},{0,0,2},{0,1,2},{1,1,4},{0,1,3},{1,1,4},{0,1,2},{0,1,2},{0,1,2},{0,0,2},{1,1,2},{0,0,2},{0,0,3},{1,1,3},{0,0,5}}))
 
 }
